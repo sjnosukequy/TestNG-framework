9eb3e973ac2072d229060b1fe459276c5e506ed6
sjnosukequy <94581993+sjnosukequy@users.noreply.github.com>
2026-01-13 00:38:31 +0700

    Add a logger to log parameters

--- src/test/java/org/QAHexagon/testng/listeners/loggerListener.java
+++ src/test/java/org/QAHexagon/testng/listeners/loggerListener.java
@@ -8,14 +8,29 @@ import org.testng.IInvokedMethod;
 import org.testng.IInvokedMethodListener;
 import org.testng.ITestResult;
 
+import java.util.Map;
+
 import org.QAHexagon.testng.utils.loggerUtils;
 
 public class loggerListener implements IInvokedMethodListener {
     private static final Logger LOGGER = LogManager.getLogger();
 
+    @SuppressWarnings("unchecked")
     @Override
     public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
         LOGGER.info("Starting: {}", method.getTestMethod().getMethodName());
+        Object[] parameters = testResult.getParameters();
+        if (parameters.length > 0) {
+            for (Object param : parameters) {
+                if (param instanceof Map) {
+                    // Special handling for Map parameters
+                    Map<String, String> row = (Map<String, String>) param;
+                    row.forEach((key, value) -> LOGGER.info("Param (key = value): {} = {}", key, value));
+                } else {
+                    LOGGER.info("Param: {}", param.toString());
+                }
+            }
+        }
     }
 
     @Override
--- src/test/java/org/QAHexagon/testng/tests/example/TestTemp.java
+++ src/test/java/org/QAHexagon/testng/tests/example/TestTemp.java
@@ -21,13 +21,14 @@ public class TestTemp extends baseTest {
 
     @DataProvider(name = "test1")
     public Iterator<Object[]> createData1() {
-        return csvHelper.readCSV("testdata/namesViet.csv");
+        return csvHelper.readCSV("testdata/names.csv");
     }
 
     @Test(dataProvider = "test1")
     public void verifyData1(Map<String, String> row) {
-        System.out.println(
-                row.get("Last Name") + " " + row.get("First Name"));
+        row.forEach((key, value) -> System.out.println(key + ": " + value));
+        // System.out.println(
+        //         row.get("Last Name") + " " + row.get("First Name"));
     }
 
 }
